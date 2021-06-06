02768a9a4d3ca32431b8c6f9fb468fd0ca8a90fc
AzureAD\동민최 <sins@kakao.com>
2021-06-06 18:47:05 +0900

    14주차

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-
     [SerializeField]
     private float moveSpeed = 10f;
 
     private float direction = 1;
 
-
+    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-
+    // Update is called once per frame
     void Update()
     {
         float speed = moveSpeed * Time.deltaTime * direction;
         Vector3 vector3 = new Vector3(speed, 0, 0);
+
         transform.Translate(vector3);
     }
 
-    public void bulletPos(float _direction)
+    public void InstantiateBullet(float _direction)
     {
         direction = _direction;
         Vector3 vector3 = new Vector3(_direction, 1, 1);
         transform.localScale = vector3;
 
-        Destroy(gameObject, 2f);
+        Destroy(this.gameObject, 2f);
     }
 }
--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,14 +1,14 @@
-using System.Collections;
+Ôªøusing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
-    private GameObject rayPos = null;
+    private float moveSpeed = 3f;
 
     [SerializeField]
-    private float moveSpeed = 3f;
+    private GameObject rayPos = null;
 
     [SerializeField]
     private int HP = 3;
@@ -16,33 +16,32 @@ public class Enemy : MonoBehaviour
     private bool moveRight = true;
 
     private bool isDead = false;
-
     void Start()
     {
 
     }
 
-
+    // Update is called once per frame
     void Update()
     {
         CheckRay();
     }
 
-    private void CheckRay()
-    {       
+    void CheckRay()
+    {
         if (isDead == false)
         {
             LayerMask layerMask = new LayerMask();
             layerMask = LayerMask.GetMask("Platform");
 
-            RaycastHit2D ray = Physics2D.Raycast(rayPos.transform.position, new Vector2(0, -1), 1.1f, layerMask.value);
+            RaycastHit2D ray = Physics2D.Raycast(rayPos.transform.position,
+                new Vector2(0, -1), 1.1f, layerMask.value);
 
             Debug.DrawRay(rayPos.transform.position, new Vector3(0, -1, 0), Color.red);
 
             if (ray == false)
             {
-                Debug.Log("π›¥ÎπÊ«‚");
-                if (moveRight == true)
+                if (moveRight)
                 {
                     moveRight = false;
                 }
@@ -55,7 +54,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void Move()
+    void Move()
     {
         float direction = 0;
         if (moveRight)
@@ -69,6 +68,7 @@ public class Enemy : MonoBehaviour
 
         Vector3 vector3 = new Vector3(direction, 1, 1);
         transform.localScale = vector3;
+
         float speed = moveSpeed * Time.deltaTime * direction;
         vector3 = new Vector3(speed, 0, 0);
         transform.Translate(vector3);
@@ -77,16 +77,30 @@ public class Enemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         bool check = collision.CompareTag("Bullet");
-
         if (check)
         {
-            HP = HP - 1; 
+            HP = HP - 1;
             if (HP < 1)
             {
                 GetComponent<Animator>().SetBool("Death", true);
                 isDead = true;
-                Destroy(gameObject, 1);
+                Destroy(this.gameObject, 1f);
             }
+        }
+ 
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            DataManager.instance.playerHP -= 1;
+            if(DataManager.instance.playerHP < 0)
+            {
+                DataManager.instance.playerHP = 0;
+            }
+            UIManager.instance.playerHP();
         }
     }
 }
+
